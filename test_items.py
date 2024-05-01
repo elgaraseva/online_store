679295bb064798663161776d681b979d0f1142c9
Elena <el.garaseva@gmail.com>
2024-05-01 23:56:45 +0300

    fix link

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_verify_button_add_to_basket(browser):
     browser.get(link)
