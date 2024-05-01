3cd22baf4c96ce4b8047b72c4aead09a6c541d15
Elena <el.garaseva@gmail.com>
2024-05-01 23:41:49 +0300

    new test_verify_button_add_to_basket

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+
+def test_verify_button_add_to_basket(browser):
+    browser.get(link)
+    browser.execute_script("window.scrollBy(0, 100);")
+    time.sleep(30)
+    buttonAddToBasket = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form .btn-add-to-basket')
+
+    assert buttonAddToBasket, "Button \"Add to busket\" not found"
